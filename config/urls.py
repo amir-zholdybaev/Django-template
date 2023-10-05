15ddd647c6a31b2958e9beed2d176891cbf00864
amir-zholdybaev <djangodev2022@gmail.com>
2023-10-05 15:22:16 +0600

    fixed the little things

--- config/urls.py
+++ config/urls.py
@@ -8,7 +8,7 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("__debug__/", include("debug_toolbar.urls")),
+    path('__debug__/', include('debug_toolbar.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
